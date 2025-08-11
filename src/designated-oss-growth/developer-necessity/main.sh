058958bdd7d141b6bce7d2ec263718f02a3ed0ad
yuichisugio <yuichisugio@users.noreply.github.com>
2025-08-11 19:04:59 +0900

    update README.md

--- src/designated-oss-growth/developer-necessity/main.sh
+++ src/designated-oss-growth/developer-necessity/main.sh
@@ -27,7 +27,7 @@ if [[ $# -gt 0 && ("$1" == "-h" || "$1" == "--help") ]]; then
 fi
 
 # データを加工するファイルを読み込む
-source "$(dirname "$0")/data-process/github-processer.sh"
+source "$(dirname "$0")/data-process/github-processor.sh"
 
 # プルリクエスト貢献者を分析。
 source "$(dirname "$0")/get-data/get-data-from-github/get-github-pull-request.sh"
