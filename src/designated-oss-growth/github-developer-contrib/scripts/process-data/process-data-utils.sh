2dace1483746cf4f68303b90248226643c5e9128
yuichisugio <yuichisugio@users.noreply.github.com>
2025-09-20 23:02:50 +0900

    update process-data/pull-request

--- src/designated-oss-growth/github-developer-contrib/scripts/process-data/process-data-utils.sh
+++ src/designated-oss-growth/github-developer-contrib/scripts/process-data/process-data-utils.sh
@@ -25,41 +25,63 @@ function process_sponsor_data() {
     '
   {
     data: {
-      user:
-        (
-          [ .[]?
-            | . as $repo
-            | .($nest_key)? as $o
-            | {
-                user_id:          ($o | .id),
-                user_database_id: ($o | .databaseId),
-                user_login:       ($o | .login),
-                user_name:        ($o | .name),
-                user_url:
-                  ( if (($o|type)=="object" and ($o|has("url")) and ($o.url!=null))
-                    then $o.url
-                    elif (($o|type)=="object" and ($o|has("login")) and ($o.login!=null))
-                      then "https://github.com/\($o.login)"
-                    else null end ),
-                task: [
-                  {
-                    task_id:               ($repo | .id),
-                    task_database_id:      ($repo | .databaseId),
-                    task_full_database_id: ($repo | .fullDatabaseId),
-                    task_name:             $task_name,
-                    task_date:             ($repo | .($task_date)),
-                    reference_task_date:   $task_date
-                  }
-                ]
-              }
-          ]
-          | sort_by(.user_id)               # group_by の前にソート
-          | group_by(.user_id)
-          | map(
-              (.[0] | {user_id, user_database_id, user_login, user_name, user_url})
-              + { task: (map(.task) | add) } # 同一ユーザーの task を結合
-            )
-        )
+      user: (
+        [ .[]?
+          | . as $obj
+          | .author as $author
+          | {
+              user_type:        $author.__typename,
+              user_id:          $author.id,
+              user_database_id: $author.databaseId,
+              user_login:       $author.login,
+              user_name:        $author.name,
+              user_url:         $author.url,
+              task: [
+                {
+                  task_id:               $obj.id,
+                  task_database_id:      $obj.databaseId,
+                  task_full_database_id: $obj.fullDatabaseId,
+                  task_url:              $obj.url,
+                  task_name:             "comment",
+                  task_date:             $obj.publishedAt,
+                  reference_task_date_field: "publishedAt",
+                  commit_word_count:     ($obj.bodyText? // "" | length),
+
+                  # content == "THUMBS_DOWN" だけを bad、それ以外は good
+                  good_reaction:
+                    (
+                      ( $obj.reactionGroups? // [] )
+                      | map(
+                        if (.content // "") == "THUMBS_DOWN"
+                          then 0
+                          else (.reactors.totalCount // 0)
+                          end
+                        )
+                      | add // 0
+                    ),
+
+                  bad_reaction:
+                    (
+                      ( $obj.reactionGroups? // [] )
+                      | map(
+                          if (.content // "") == "THUMBS_DOWN"
+                          then (.reactors.totalCount // 0)
+                          else 0
+                          end
+                        )
+                      | add // 0
+                    )
+                }
+              ]
+            }
+        ]
+        | sort_by(.user_id)
+        | group_by(.user_id)
+        | map(
+            (.[0] | {user_id, user_database_id, user_login, user_name, user_url} )
+            + { task: (map(.task) | add) }   # 同一ユーザーの task を結合
+          )
+      )
     }
   }
     ' "$INPUT_PATH" \
